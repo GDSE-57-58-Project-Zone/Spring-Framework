f173e48b798bb010d197969bed2ab0631b29e7c4
sanuv9683 <sanuv9683@gmail.com>
2022-05-13 16:00:42 +0530

    Initial Commit

---
+++ AAD_SPRING/01_Core/src/AppInitializer.java
@@ -0,0 +1,17 @@
+import config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author : Sanu Vithanage
+ * @since : 0.1.0
+ **/
+public class AppInitializer {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext ctx= new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
+        ctx.close();
+
+    }
+}
---
+++ AAD_SPRING/01_Core/src/config/AppConfig.java
@@ -0,0 +1,14 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : Sanu Vithanage
+ * @since : 0.1.0
+ **/
+
+@Configuration
+@ComponentScan(basePackages = "bean")
+public class AppConfig {
+}
