2ec9e3d3130a84d67af0b2ffecaa9d8703c734cd
sanuv9683 <sanuv9683@gmail.com>
2022-05-18 09:39:12 +0530

    update commits

--- AAD_SPRING/01_Core/src/AppInitializer.java
+++ AAD_SPRING/01_Core/src/AppInitializer.java
@@ -1,6 +1,4 @@
-import bean.SpringBeanOne;
 import bean.SpringBeanThree;
-import bean.SpringBeanTwo;
 import config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -13,14 +11,14 @@ public class AppInitializer {
 
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
 
-//        //Run something just before jvm shutdown
-//        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                System.out.println("JVM is about to Shut Down");
-//                ctx.close();
-//            }
-//        }));
+        //Run something just before jvm shutdown
+        //Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+        //   @Override
+        //  public void run() {
+        //     System.out.println("JVM is about to Shut Down");
+        //        ctx.close();
+        //    }
+        //}));
 
         ctx.registerShutdownHook();
 
@@ -28,20 +26,26 @@ public class AppInitializer {
         ctx.register(AppConfig.class);
         ctx.refresh();
 
-
-        SpringBeanOne beanOne = ctx.getBean(SpringBeanOne.class);
-        SpringBeanTwo beanTwo = ctx.getBean(SpringBeanTwo.class);
-        System.out.println(beanOne);
-        System.out.println(beanTwo);
-
+        //SpringBeanOne beanOne = ctx.getBean(SpringBeanOne.class);
+        //SpringBeanTwo beanTwo = ctx.getBean(SpringBeanTwo.class); // class type
+        //System.out.println(beanOne);
+        //System.out.println(beanTwo);
+        //
         SpringBeanThree beanThree = ctx.getBean(SpringBeanThree.class);
         System.out.println(beanThree);
 
+        //Bean Name request (Bean ID)
+        //SpringBeanOne->  springBeanOne (Bean ID)
+        //SpringBeanOne springBeanOne = (SpringBeanOne) ctx.getBean("springBeanOne");
+        //System.out.println(springBeanOne);
 
+        //SpringBeanTwo -> springBeanTwo
+        //SpringBeanTwo springBeanTwo = (SpringBeanTwo) ctx.getBean("springBeanTwo");//bean id
+        //System.out.println(springBeanTwo);
 
-
-
-
+        //SpringBeanThree -> springBeanThree
+        SpringBeanThree springBeanThree = (SpringBeanThree) ctx.getBean("springBeanThree");
+        System.out.println(springBeanThree);
 
     }
 }
