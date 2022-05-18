2e118f8045b1b0edc768d54300d29929c20a12a0
sanuv9683 <sanuv9683@gmail.com>
2022-05-18 09:17:53 +0530

    Register ShutDown hook method introduced

--- AAD_SPRING/01_Core/src/AppInitializer.java
+++ AAD_SPRING/01_Core/src/AppInitializer.java
@@ -11,18 +11,21 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class AppInitializer {
     public static void main(String[] args) {
 
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("JVM is about to Shut Down");
-            }
-        }));
-
-
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
 
-        ctx.register(AppConfig.class);
+//        //Run something just before jvm shutdown
+//        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("JVM is about to Shut Down");
+//                ctx.close();
+//            }
+//        }));
 
+        ctx.registerShutdownHook();
+
+
+        ctx.register(AppConfig.class);
         ctx.refresh();
 
 
@@ -34,7 +37,7 @@ public class AppInitializer {
         SpringBeanThree beanThree = ctx.getBean(SpringBeanThree.class);
         System.out.println(beanThree);
 
-        ctx.close();
+
 
 
 
