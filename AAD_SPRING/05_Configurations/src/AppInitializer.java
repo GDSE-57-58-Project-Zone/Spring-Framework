e10722361240e26d7871dbe907e8f0f3a1ddc459
sanuv9683 <sanuv9683@gmail.com>
2022-05-23 10:40:29 +0530

    Content Updated

--- AAD_SPRING/05_Configurations/src/AppInitializer.java
+++ AAD_SPRING/05_Configurations/src/AppInitializer.java
@@ -11,8 +11,8 @@ public class AppInitializer {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context= new AnnotationConfigApplicationContext();
         context.register(AppConfig.class);
-        context.register(AppConfig1.class);
-        context.register(AppConfig2.class);
+//        context.register(AppConfig2.class);
+//        context.register(AppConfig1.class);
 
         context.refresh();
         context.registerShutdownHook();
--- AAD_SPRING/05_Configurations/src/config/AppConfig.java
+++ AAD_SPRING/05_Configurations/src/config/AppConfig.java
@@ -2,6 +2,8 @@ package config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * @author : Sanu Vithanage
@@ -11,6 +13,9 @@ import org.springframework.context.annotation.Configuration;
 //A source for Spring Bean Definitions
 @Configuration
 @ComponentScan(basePackages = "bean")
+@Import({AppConfig1.class,AppConfig2.class})// we can import configurations into a single class
+//@ImportResource("classpath:abcd.xml") // we can use this annotation to import xml configurations to the context
+//@ImportResource("file:absolute-path-of-abcd.xml")
 public class AppConfig {
 
 }
