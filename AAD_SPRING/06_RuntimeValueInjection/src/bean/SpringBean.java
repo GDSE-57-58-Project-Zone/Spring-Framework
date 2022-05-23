d5afad3387b1c6a9957795d74722a52666bb5d2b
sanuv9683 <sanuv9683@gmail.com>
2022-05-23 11:23:55 +0530

    Introducing autowired annotation for costructors and applying required false

--- AAD_SPRING/06_RuntimeValueInjection/src/bean/SpringBean.java
+++ AAD_SPRING/06_RuntimeValueInjection/src/bean/SpringBean.java
@@ -1,5 +1,6 @@
 package bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -10,8 +11,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class SpringBean {
 
-    public SpringBean(@Value("C001") String id) {
+
+//    public SpringBean() {
+//        System.out.println("Default Constructor");
+//    }
+
+    @Autowired(required = false)
+    public SpringBean(@Value("C001") String id, @Value("10") int age, @Value("true") boolean b) {
         System.out.println("Spring Bean Instantiated");
         System.out.println(id);
+        System.out.println(age);
+        System.out.println(b);
+    }
+
+
+    @Autowired(required = false)
+    public SpringBean(@Value("1,2,3") int[] myNames,@Value("A") char a) {
+        System.out.println("Spring Bean Instantiated");
+        for (Integer myName : myNames) {
+            System.out.println(myName);
+        }
+        System.out.println(a);
     }
 }
