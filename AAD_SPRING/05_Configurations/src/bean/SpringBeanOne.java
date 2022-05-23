86efdca0bf8b7f4df3e678baf029b5fc0e0cb8e5
sanuv9683 <sanuv9683@gmail.com>
2022-05-23 10:07:12 +0530

    Spring Configurations Introduced

---
+++ AAD_SPRING/05_Configurations/src/bean/SpringBeanOne.java
@@ -0,0 +1,15 @@
+package bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : Sanu Vithanage
+ * @since : 0.1.0
+ **/
+
+@Component
+public class SpringBeanOne {
+    public SpringBeanOne() {
+        System.out.println("Spring Bean Instantiated");
+    }
+}
