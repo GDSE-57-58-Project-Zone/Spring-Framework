2e747dc7cc434b4fa9eabf876a815b246bb1abc7
sanuv9683 <sanuv9683@gmail.com>
2022-05-27 10:20:45 +0530

    Updated the WebAppConfig class

--- AAD_SPRING/08_Spring_MVC/src/config/WebAppConfig.java
+++ AAD_SPRING/08_Spring_MVC/src/config/WebAppConfig.java
@@ -1,8 +1,16 @@
 package config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /**
  * @author : Sanu Vithanage
  * @since : 0.1.0
  **/
+
+@Configuration
+@ComponentScan(basePackages = "bean")
+@EnableWebMvc
 public class WebAppConfig {
 }
