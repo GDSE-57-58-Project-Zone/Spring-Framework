8fc4a7c42789c028c2923057972c7191eeb6fd45
sanuv9683 <sanuv9683@gmail.com>
2022-05-27 10:32:54 +0530

    Aded servlet API to the Spring MVC project and run

--- AAD_SPRING/08_Spring_MVC/src/config/WebRootConfig.java
+++ AAD_SPRING/08_Spring_MVC/src/config/WebRootConfig.java
@@ -1,8 +1,12 @@
 package config;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author : Sanu Vithanage
  * @since : 0.1.0
  **/
+@Configuration
 public class WebRootConfig {
+
 }
