55a083c18c37899c39c742990ef712c4e7eb9525
sanuv9683 <sanuv9683@gmail.com>
2022-05-30 10:21:08 +0530

    Aded Another handler method

--- AAD_SPRING/09_RestControllers/src/lk/ijse/spring/controller/CustomerController.java
+++ AAD_SPRING/09_RestControllers/src/lk/ijse/spring/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package lk.ijse.spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author : Sanu Vithanage
  * @since : 0.1.0
  **/
-
+//REST = Representational State Transfer (A way of data transfer among client server apps)
 @RestController
 @RequestMapping("customer")
 public class CustomerController {
@@ -16,5 +17,10 @@ public class CustomerController {
     @GetMapping
     public String getAllCustomer(){
         return "Hello There..!";
+    }
+
+    @PostMapping
+    public String saveCustomer(){
+        return "Customer Saved and Post Method Invoked";
     }
 }
