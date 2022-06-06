9a3693fd80e11379257d4368685c3241ac88934f
sanuv9683 <sanuv9683@gmail.com>
2022-06-06 08:55:46 +0530

    Spring Params

---
+++ AAD_SPRING/09_RestControllers/src/lk/ijse/spring/controller/ParamsController.java
@@ -0,0 +1,26 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Sanu Vithanage
+ * @since : 0.1.0
+ **/
+@RestController
+@RequestMapping("params")
+public class ParamsController {
+
+
+    @GetMapping(params = {"param1","param2"})
+    public String test1() {
+        return "Hello 1";
+    }
+
+    @GetMapping
+    public String test2() {
+        return "Hello 2";
+    }
+
+}
