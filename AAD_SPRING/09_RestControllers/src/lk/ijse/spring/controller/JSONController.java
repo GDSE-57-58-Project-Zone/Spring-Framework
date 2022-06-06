0580bb5339762661a01184f74f901409d5e58dfd
sanuv9683 <sanuv9683@gmail.com>
2022-06-06 10:26:26 +0530

    Content Updated with Json response

--- AAD_SPRING/09_RestControllers/src/lk/ijse/spring/controller/JSONController.java
+++ AAD_SPRING/09_RestControllers/src/lk/ijse/spring/controller/JSONController.java
@@ -1,5 +1,9 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.CustomerDTO;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("json")
 public class JSONController {
 
+    @PostMapping
+    public String getJsonRequest(CustomerDTO dto) {
+        return "Hello JSON " + dto.toString();
+    }
+
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    public CustomerDTO sendBackJSON() {
+        return new CustomerDTO("C001","Dasun","Galle",100.00);
+    }
 
 }
