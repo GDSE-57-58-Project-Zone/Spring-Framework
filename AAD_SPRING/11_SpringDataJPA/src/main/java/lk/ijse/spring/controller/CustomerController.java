bfa4a8a2f3631d9efdd4dad546779671d441b759
sanuv9683 <sanuv9683@gmail.com>
2022-06-10 15:36:04 +0530

    Introduce new module for Spring Data JPA

---
+++ AAD_SPRING/11_SpringDataJPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -0,0 +1,21 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Sanu Vithanage
+ * @since : 0.1.0
+ **/
+@RestController
+@RequestMapping("api/v1/customer")
+@CrossOrigin
+public class CustomerController {
+
+    @GetMapping
+    public String getAllCustomers() {
+        return "Hello app is working";
+    }
+}
