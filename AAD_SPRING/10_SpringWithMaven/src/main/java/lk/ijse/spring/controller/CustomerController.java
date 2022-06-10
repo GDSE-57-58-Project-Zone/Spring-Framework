4db7d9b0467f8ce8aee16647c7544504ed8fea52
sanuv9683 <sanuv9683@gmail.com>
2022-06-10 14:14:46 +0530

    resolve the CROS error

--- AAD_SPRING/10_SpringWithMaven/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ AAD_SPRING/10_SpringWithMaven/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,8 +1,10 @@
 package lk.ijse.spring.controller;
 
 import lk.ijse.spring.dto.CustomerDTO;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
@@ -11,11 +13,16 @@ import java.util.ArrayList;
  * @since : 0.1.0
  **/
 @RequestMapping("api/v1/customer")
+@RestController
+@CrossOrigin
 public class CustomerController {
 
     @GetMapping
     public ArrayList<CustomerDTO> getAllCustomers(){
         ArrayList<CustomerDTO> arrayList= new ArrayList<>();
+        arrayList.add(new CustomerDTO("C001","Dasun","Galle",1000));
+        arrayList.add(new CustomerDTO("C002","Kasun","Galle",1000));
+        arrayList.add(new CustomerDTO("C003","Dananjaya","Galle",1000));
         return arrayList;
     }
 }
