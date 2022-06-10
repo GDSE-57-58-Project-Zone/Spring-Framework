acb8b835ad190235e98fb607cd98e03af371cec7
sanuv9683 <sanuv9683@gmail.com>
2022-06-10 13:04:14 +0530

    Content Updated

--- AAD_SPRING/10_SpringWithMaven/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ AAD_SPRING/10_SpringWithMaven/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.CustomerDTO;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("customer")
 public class CustomerController {
 
-    @GetMapping
-    public String getAllCustomers() {
-        return "Hello Maven";
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public CustomerDTO getAllCustomers() {
+        return new CustomerDTO("C001","IJSE","Panadura",1000);
     }
 
 
