0af02d8304116331ca3d05706eef7ff6bba751fe
sanuv9683 <sanuv9683@gmail.com>
2022-06-10 12:55:50 +0530

    Content Updated with maven

---
+++ AAD_SPRING/10_SpringWithMaven/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -0,0 +1,21 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Sanu Vithanage
+ * @since : 0.1.0
+ **/
+@RestController
+@RequestMapping("customer")
+public class CustomerController {
+
+    @GetMapping
+    public String getAllCustomers() {
+        return "Hello Maven";
+    }
+
+
+}
