3b6164da365b6edaafcb97d475d25bda1e36c9b2
sanuv9683 <sanuv9683@gmail.com>
2022-06-10 16:10:02 +0530

    Content Updated

--- AAD_SPRING/11_SpringDataJPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ AAD_SPRING/11_SpringDataJPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -2,8 +2,12 @@ package lk.ijse.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.sql.DataSource;
 
 /**
  * @author : Sanu Vithanage
@@ -13,11 +17,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class JPAConfig {
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter va){
         LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
-        bean.setJpaVendorAdapter(); // Vendor
-        bean.setDataSource(); //Connection
+        bean.setJpaVendorAdapter(va); // Vendor (Hibernate)
+        bean.setDataSource(ds); //Connection
         bean.setPackagesToScan("lk.ijse.spring.entity"); // location of the entity
         return bean;
     }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource= new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/springjpa?createDatabaseIfNotExist=true");
+        dataSource.setUsername("root");
+        dataSource.setPassword("sanu");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        return dataSource;
+    }
+
 }
