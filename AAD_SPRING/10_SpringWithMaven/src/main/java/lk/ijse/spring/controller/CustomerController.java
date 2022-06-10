b9805dd6c7281a5b5d1f59ed0cf1583ef4d6624b
sanuv9683 <sanuv9683@gmail.com>
2022-06-10 14:49:53 +0530

    Contnet Updated

--- AAD_SPRING/10_SpringWithMaven/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ AAD_SPRING/10_SpringWithMaven/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,10 +1,8 @@
 package lk.ijse.spring.controller;
 
 import lk.ijse.spring.dto.CustomerDTO;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -18,11 +16,32 @@ import java.util.ArrayList;
 public class CustomerController {
 
     @GetMapping
-    public ArrayList<CustomerDTO> getAllCustomers(){
-        ArrayList<CustomerDTO> arrayList= new ArrayList<>();
-        arrayList.add(new CustomerDTO("C001","Dasun","Galle",1000));
-        arrayList.add(new CustomerDTO("C002","Kasun","Galle",1000));
-        arrayList.add(new CustomerDTO("C003","Dananjaya","Galle",1000));
+    public ArrayList<CustomerDTO> getAllCustomers() {
+        ArrayList<CustomerDTO> arrayList = new ArrayList<>();
+        arrayList.add(new CustomerDTO("C001", "Dasun", "Galle", 1000));
+        arrayList.add(new CustomerDTO("C002", "Kasun", "Galle", 1000));
+        arrayList.add(new CustomerDTO("C003", "Dananjaya", "Galle", 1000));
         return arrayList;
     }
+
+    @GetMapping(path = "/{id}")
+    public CustomerDTO searchCustomer(@PathVariable String id) {
+        return new CustomerDTO(id, "Dausn", "Galle", 1000);
+    }
+
+
+    @PutMapping(consumes ={MediaType.APPLICATION_JSON_VALUE} )
+    public CustomerDTO updateCustomer(@RequestBody CustomerDTO dto) {
+        System.out.println(dto.toString());
+        return dto;
+    }
+
+
+    @PostMapping(consumes = {"application/x-www-form-urlencoded"})
+    public CustomerDTO saveCustomer(@ModelAttribute CustomerDTO dto) {
+        System.out.println(dto.toString());
+        return dto;
+    }
+
+
 }
