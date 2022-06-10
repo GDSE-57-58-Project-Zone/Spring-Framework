aa4df29ec38786a3721f926a07fd951c490f8b69
sanuv9683 <sanuv9683@gmail.com>
2022-06-10 15:54:37 +0530

    Introduced the LocalContainerEntityManagerBeanFactory

--- AAD_SPRING/11_SpringDataJPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ AAD_SPRING/11_SpringDataJPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -1,6 +1,9 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * @author : Sanu Vithanage
@@ -8,4 +11,13 @@ import org.springframework.context.annotation.Configuration;
  **/
 @Configuration
 public class JPAConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+        LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+        bean.setJpaVendorAdapter(); // Vendor
+        bean.setDataSource(); //Connection
+        bean.setPackagesToScan("lk.ijse.spring.entity"); // location of the entity
+        return bean;
+    }
 }
