b665ecb0c19ef81f1c6fb50fb6425276e882bbb8
sanuv9683 <sanuv9683@gmail.com>
2022-06-13 11:30:14 +0530

    Aded loos coupling

--- AAD_SPRING/11_SpringDataJPA/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
+++ AAD_SPRING/11_SpringDataJPA/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
@@ -1,7 +1,8 @@
-package lk.ijse.spring.service;
+package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.repo.CustomerRepo;
+import lk.ijse.spring.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,11 +15,12 @@ import java.util.List;
  **/
 @Service
 @Transactional
-public class CustomerServiceImpl {
+public class CustomerServiceImpl implements CustomerService {
 
     @Autowired
     private CustomerRepo repo;
 
+    @Override
     public void saveCustomer(Customer entity) {
         if (!repo.existsById(entity.getId())){
             repo.save(entity);
@@ -28,18 +30,22 @@ public class CustomerServiceImpl {
 
     }
 
+    @Override
     public void deleteCustomer(String id) {
         repo.deleteById(id);
     }
 
+    @Override
     public void updateCustomer(Customer entity) {
         repo.save(entity);
     }
 
+    @Override
     public Customer searchCustomer(String id) {
         return repo.findById(id).get();
     }
 
+    @Override
     public List<Customer> getAllCustomers() {
         return repo.findAll();
     }
