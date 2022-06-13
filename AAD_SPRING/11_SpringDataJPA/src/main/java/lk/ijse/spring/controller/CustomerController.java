de1b934d7864b48e94f561a3a8e17e176e1a8522
sanuv9683 <sanuv9683@gmail.com>
2022-06-13 11:11:54 +0530

    Updated content

--- AAD_SPRING/11_SpringDataJPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ AAD_SPRING/11_SpringDataJPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,11 +1,14 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.service.CustomerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author : Sanu Vithanage
@@ -20,7 +23,7 @@ public class CustomerController {
     CustomerServiceImpl customerService;
 
     @GetMapping
-    public String getAllCustomers() {
-        return "Hello app is working";
+    public List<Customer> getAllCustomers() {
+        return customerService.getAllCustomers();
     }
 }
--- AAD_SPRING/11_SpringDataJPA/src/main/java/lk/ijse/spring/service/CustomerServiceImpl.java
+++ AAD_SPRING/11_SpringDataJPA/src/main/java/lk/ijse/spring/service/CustomerServiceImpl.java
@@ -3,7 +3,6 @@ package lk.ijse.spring.service;
 import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.repo.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
