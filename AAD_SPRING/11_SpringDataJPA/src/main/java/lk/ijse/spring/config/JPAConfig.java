2382654e3712e6f869e48852a47a5410b1a73e64
sanuv9683 <sanuv9683@gmail.com>
2022-06-13 08:33:24 +0530

    Introduced the transaction manager

--- AAD_SPRING/11_SpringDataJPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ AAD_SPRING/11_SpringDataJPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -3,12 +3,15 @@ package lk.ijse.spring.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 /**
@@ -46,5 +49,11 @@ public class JPAConfig {
         vendor.setGenerateDdl(true);
         return vendor;
     }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+        return new JpaTransactionManager(emf);
+    }
+
 
 }
