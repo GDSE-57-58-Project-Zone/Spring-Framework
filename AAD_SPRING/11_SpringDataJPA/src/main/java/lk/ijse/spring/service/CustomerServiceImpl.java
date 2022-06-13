e341ff9a9276df97174770afa8baaa931e4b780e
sanuv9683 <sanuv9683@gmail.com>
2022-06-13 11:02:14 +0530

    Aded two annotattions

--- AAD_SPRING/11_SpringDataJPA/src/main/java/lk/ijse/spring/service/CustomerServiceImpl.java
+++ AAD_SPRING/11_SpringDataJPA/src/main/java/lk/ijse/spring/service/CustomerServiceImpl.java
@@ -3,6 +3,9 @@ package lk.ijse.spring.service;
 import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.repo.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -10,6 +13,8 @@ import java.util.List;
  * @author : Sanu Vithanage
  * @since : 0.1.0
  **/
+@Service
+@Transactional
 public class CustomerServiceImpl {
 
     @Autowired
