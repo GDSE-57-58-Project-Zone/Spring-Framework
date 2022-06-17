f7c0334bc9d59ef332e1dd9300d508895dd898f3
sanuv9683 <sanuv9683@gmail.com>
2022-06-17 08:44:32 +0530

    Resolve Errors

--- AAD_SPRING/12_Model_Mapper/src/main/java/lk/ijse/spring/advisor/AppWideExceptionHandler.java
+++ AAD_SPRING/12_Model_Mapper/src/main/java/lk/ijse/spring/advisor/AppWideExceptionHandler.java
@@ -1,6 +1,8 @@
 package lk.ijse.spring.advisor;
 
 import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,7 +17,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class AppWideExceptionHandler {
 
     @ExceptionHandler({Exception.class})
-    public ResponseUtil exceptionHandler(Exception e) {
-       return new ResponseUtil(500, e.getMessage(), null);
+    public ResponseEntity exceptionHandler(Exception e) {
+        //return new ResponseUtil(500, e.getMessage(), null);
+        return new ResponseEntity(new ResponseUtil(500, e.getMessage(), null), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
