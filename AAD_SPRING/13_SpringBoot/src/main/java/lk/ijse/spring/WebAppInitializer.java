a4ef3047e6e9808fca9e2238661961b84f0d6b30
sanuv9683 <sanuv9683@gmail.com>
2022-06-20 12:03:36 +0530

    Content Updated With Dev Tools

--- AAD_SPRING/13_SpringBoot/src/main/java/lk/ijse/spring/WebAppInitializer.java
+++ AAD_SPRING/13_SpringBoot/src/main/java/lk/ijse/spring/WebAppInitializer.java
@@ -9,9 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  **/
 @SpringBootApplication
 public class WebAppInitializer {
-
     public static void main(String[] args) {
         SpringApplication.run(WebAppInitializer.class);
     }
-
 }
