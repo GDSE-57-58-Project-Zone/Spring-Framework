a8d3971b8f0765a84faef2e0d2d712f713c195f7
sanuv9683 <sanuv9683@gmail.com>
2022-06-20 11:41:04 +0530

    Updated the WebAppInitializer

--- AAD_SPRING/13_SpringBoot/src/main/java/lk/ijse/spring/WebAppInitializer.java
+++ AAD_SPRING/13_SpringBoot/src/main/java/lk/ijse/spring/WebAppInitializer.java
@@ -1,26 +1,17 @@
 package lk.ijse.spring;
 
-import lk.ijse.spring.config.WebAppConfig;
-import lk.ijse.spring.config.WebRootConfig;
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author : Sanu Vithanage
  * @since : 0.1.0
  **/
-public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[]{WebRootConfig.class};
+@SpringBootApplication
+public class WebAppInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppInitializer.class);
     }
 
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class[]{WebAppConfig.class};
-    }
-
-    @Override
-    protected String[] getServletMappings() {
-        return new String[]{"/"};
-    }
 }
